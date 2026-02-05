97c166317d5a0bf21f22963bde5541cee7c247f5
Rajdeep Dhar <rajdeepdhar.dev@gmail.com>
2026-02-05 16:41:36 +0530

    edit 3

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ import { inngest } from "./lib/inngest.js";
 import { functions } from "./lib/inngest.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
-import chatRoutes from "./routes/chatRoutes.js"
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -17,9 +17,10 @@ const __dirname = path.dirname(__filename);
 //middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.use(clerkMiddleware()); //req.auth()
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 
 if (ENV.NODE_ENV === "production") {
